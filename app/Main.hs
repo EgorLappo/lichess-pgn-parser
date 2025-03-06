217524825aecb739b31a15e6293c6a1d331e7852
Egor Lappo <elappo@stanford.edu>
2025-03-06 03:36:07 +0300

    formatting, lint fixing

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,32 @@
 module Main (main) where
 
-import           Data.Text            (Text)
-import qualified Data.Text            as T
-import qualified Data.Text.IO         as T
-
-import qualified Data.ByteString.Lazy as BSL
-
-import           Options.Applicative
-
-import           Lib
+import Control.Monad (when)
+import Data.Text (Text)
+import Lib
+import Options.Applicative
 
 data Options = Options
-  {
-    keep :: !Bool
-  , hd   :: !Bool
-  , sep  :: !Text
-  , cut  :: !Int
+  { keep :: !Bool,
+    hd :: !Bool,
+    sep :: !Text,
+    cut :: !Int
   }
 
 optionsParser :: ParserInfo Options
 optionsParser =
-    info (helper <*> options)
-      ( fullDesc
-     <> progDesc "convert a lichess pgn file into tabular format, streaming"
-     <> header "lichess-pgn-parser. featuring: attoparsec" )
+  info
+    (helper <*> options)
+    ( fullDesc
+        <> progDesc "convert a lichess pgn file into tabular format, streaming"
+        <> header "lichess-pgn-parser. featuring: attoparsec"
+    )
   where
-    options = Options <$>
-      switch ( long "keep" <> short 'k' <> help "keep the move string as-is (e.g. with time, evaluation data)" ) <*>
-      switch ( long "header" <> short 'n' <> help "print the csv header line (column names)" ) <*>
-      option auto ( long "sep" <> short 's' <> help "separator character for output (default: ,)" <> value "," ) <*>
-      option auto ( long "cut" <> short 'c' <> help "cut the move string after ARG moves, making individual columns  (default: 0)" <> value 0 )
+    options =
+      Options
+        <$> switch (long "keep" <> short 'k' <> help "keep the move string as-is (e.g. with time, evaluation data)")
+        <*> switch (long "header" <> short 'n' <> help "print the csv header line (column names)")
+        <*> option auto (long "sep" <> short 's' <> help "separator character for output (default: ,)" <> value ",")
+        <*> option auto (long "cut" <> short 'c' <> help "cut the move string after ARG moves, making individual columns  (default: 0)" <> value 0)
 
 -- test out the options
 main :: IO ()
@@ -40,17 +37,11 @@ main = do
       h = hd opts
       s = sep opts
 
-  if (ncut > 0) && k then
-    error "cannot use both --keep and --cut"
-  else return ()
+  when ((ncut > 0) && k) $ error "cannot use both --keep and --cut"
 
-  if k then runKeep h s
-  else if (ncut > 0) then runCut ncut h s
-  else runDef h s
-
-
-
-
-
-
-
+  if k
+    then runKeep h s
+    else
+      if ncut > 0
+        then runCut ncut h s
+        else runDef h s
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,102 +1,103 @@
-module Lib where
+module Lib (runKeep, runCut, runDef) where
 
-import           Control.Applicative              ((<|>))
-import           Data.Map                         ((!))
-import qualified Data.Map                         as M
-import           Data.Maybe                       (maybe)
-
-import           Data.Text                        (Text)
-import qualified Data.Text                        as T
-import           Data.Text.Encoding               (decodeLatin1)
-import qualified Data.Text.IO                     as T
-
-import qualified Data.ByteString.Lazy             as BSL
-
-import           Data.Attoparsec.ByteString.Char8
-import qualified Data.Attoparsec.ByteString.Lazy  as Lazy
-import qualified Data.Attoparsec.Text             as TextP
-
-import           Replace.Attoparsec.Text          (streamEdit)
+import Control.Applicative ((<|>))
+import Control.Monad (when)
+import Data.Attoparsec.ByteString.Char8
+import Data.Attoparsec.ByteString.Lazy qualified as Lazy
+import Data.Attoparsec.Text qualified as TextP
+import Data.ByteString.Lazy qualified as BSL
+import Data.Map ((!))
+import Data.Map qualified as Map
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding (decodeLatin1)
+import Data.Text.IO qualified as T
+import Replace.Attoparsec.Text (streamEdit)
 
 -- https://stackoverflow.com/questions/27236539/what-about-data-attoparsec-bytestring-lazy-char8
 
-
 data PGN = PGN
-  { site        :: Text
-  , date        :: Text
-  , whitePlayer :: Text
-  , blackPlayer :: Text
-  , whiteElo    :: Text
-  , blackElo    :: Text
-  , whiteTitle  :: Text
-  , blackTitle  :: Text
-  , eco         :: Text
-  , timeControl :: Text
-  , result      :: Text
-  , termination :: Text
-  , variant     :: Text
-  , moves       :: Text
-  } deriving Show
+  { site :: Text,
+    date :: Text,
+    whitePlayer :: Text,
+    blackPlayer :: Text,
+    whiteElo :: Text,
+    blackElo :: Text,
+    whiteTitle :: Text,
+    blackTitle :: Text,
+    eco :: Text,
+    timeControl :: Text,
+    result :: Text,
+    termination :: Text,
+    variant :: Text,
+    moves :: Text
+  }
+  deriving (Show)
 
 -- generic function to run the parser from stdin
-run
-  :: (PGN -> PGN) -- optional function to process the PGN before formatting to CSV
-  -> Text -- header line with column names
-  -> Bool -- whether to print the header line
-  -> Text -- separator character as Text
-  -> IO ()
-run f colNames h sep = do
-    if h then T.putStrLn colNames else return ()
-    lines <- BSL.getContents
-    goRec (Lazy.parse game lines)
+run ::
+  (PGN -> PGN) -> -- optional function to process the PGN before formatting to CSV
+  Text -> -- header line with column names
+  Bool -> -- whether to print the header line
+  Text -> -- separator character as Text
+  IO ()
+run f colNames' h sep = do
+  when h $ T.putStrLn colNames'
+  flines <- BSL.getContents
+  goRec (Lazy.parse game flines)
   where
     goRec :: Lazy.Result PGN -> IO ()
     goRec (Lazy.Fail _ _ e) = error $ "parser failed: " <> e
-    goRec (Lazy.Done bs pgn) = if (BSL.null bs)
-      then do
-        T.putStrLn . formatToCSV sep . processPGN . f $ pgn
-      else do
-        T.putStrLn . formatToCSV sep . processPGN . f $ pgn
-        goRec (Lazy.parse game bs)
+    goRec (Lazy.Done bs pgn) =
+      if BSL.null bs
+        then T.putStrLn . formatToCSV sep . processPGN . f $ pgn
+        else do
+          T.putStrLn . formatToCSV sep . processPGN . f $ pgn
+          goRec (Lazy.parse game bs)
 
 runKeep h sep = run id colNames' h sep
-  where colNames' = (T.intercalate sep colNames) <> sep <> "moves"
+  where
+    colNames' = T.intercalate sep colNames <> sep <> "moves"
 
 runDef h sep = run (cleanMoves 0) colNames' h sep
-  where colNames' = (T.intercalate sep colNames) <> sep <> "moves"
+  where
+    colNames' = T.intercalate sep colNames <> sep <> "moves"
 
 runCut ncut h sep = run (cleanMoves ncut) colNames' h sep
-  where colNames' = T.intercalate sep colNames <> sep <> T.intercalate sep (map (\i -> "move" <> T.pack (show i)) [1..ncut])
+  where
+    colNames' = T.intercalate sep colNames <> sep <> T.intercalate sep (map (\i -> "move" <> T.pack (show i)) [1 .. ncut])
 
 -- i borrow a lot from https://hackage.haskell.org/package/chesshs here
 -- in terms of parsing the tags and the whole PGN type
 
 game = do
-    tags <- M.fromList <$> many1 tag
-    skipSpace
-    moves' <- moveLine
-    skipSpace
-    return $ PGN (decodeLatin1 $ tags ! "Site")
-                (decodeLatin1 $ tags ! "UTCDate")
-                (decodeLatin1 $ tags ! "White")
-                (decodeLatin1 $ tags ! "Black")
-                (decodeLatin1 $ tags ! "WhiteElo")
-                (decodeLatin1 $ tags ! "BlackElo")
-                (optional tags "WhiteTitle")
-                (optional tags "BlackTitle")
-                (decodeLatin1 $ tags ! "ECO")
-                (decodeLatin1 $ tags ! "TimeControl")
-                (decodeLatin1 $ tags ! "Result")
-                (decodeLatin1 $ tags ! "Termination")
-                (optional tags "Variant")
-                (decodeLatin1 moves')
+  tags <- Map.fromList <$> many1 tag
+  skipSpace
+  moves' <- moveLine
+  skipSpace
+  return $
+    PGN
+      (decodeLatin1 $ tags ! "Site")
+      (decodeLatin1 $ tags ! "UTCDate")
+      (decodeLatin1 $ tags ! "White")
+      (decodeLatin1 $ tags ! "Black")
+      (decodeLatin1 $ tags ! "WhiteElo")
+      (decodeLatin1 $ tags ! "BlackElo")
+      (optional tags "WhiteTitle")
+      (optional tags "BlackTitle")
+      (decodeLatin1 $ tags ! "ECO")
+      (decodeLatin1 $ tags ! "TimeControl")
+      (decodeLatin1 $ tags ! "Result")
+      (decodeLatin1 $ tags ! "Termination")
+      (optional tags "Variant")
+      (decodeLatin1 moves')
   where
-    optional t v = maybe "" decodeLatin1 (M.lookup v t)
+    optional t v = maybe "" decodeLatin1 (Map.lookup v t)
 
 tag = do
   skipSpace
   char '['
-  tagName <- takeTill ((==) ' ')
+  tagName <- takeTill (' ' ==)
   skipSpace
   tagValue <- stringLiteral
   char ']'
@@ -104,76 +105,79 @@ tag = do
 
 stringLiteral = do
   char '"'
-  value <- takeTill ((==) '"')
+  value <- takeTill ('"' ==)
   char '"'
   return value
 
 moveLine = do
-    skipSpace
-    moves' <- takeTill ((==) '\n')
-    char '\n'
-    return moves'
+  skipSpace
+  moves' <- takeTill ('\n' ==)
+  char '\n'
+  return moves'
 
 cleanMoves :: Int -> PGN -> PGN
 cleanMoves 0 pgn =
-    pgn { moves = moves' }
+  pgn {moves = moves'}
   where
     -- parse the move string omitting anything between curly braces {}
-    moves' = (T.unwords . map stripMarks . init . T.words) $ streamEdit (extraInfo <|> moveNumber) (const "") (moves pgn)
-    extraInfo = TextP.char '{' *> TextP.takeTill ((==) '}') <* TextP.char '}'
+    moves' = T.unwords . map stripMarks . init . T.words $ streamEdit (extraInfo <|> moveNumber) (const "") (moves pgn)
+    extraInfo = TextP.char '{' *> TextP.takeTill ('}' ==) <* TextP.char '}'
     moveNumber = do
-        TextP.decimal
-        (TextP.many1 $ TextP.char '.')
-        return ""
+      (_ :: Int) <- TextP.decimal
+      TextP.many1 $ TextP.char '.'
+      return ""
     stripMarks = T.filter (\c -> c /= '?' && c /= '!')
 cleanMoves n pgn =
-    pgn { moves = moves'' }
+  pgn {moves = moves''}
   where
     -- parse the move string omitting anything between curly braces {}
-    mlist = (map stripMarks . init . T.words) $ streamEdit (extraInfo <|> moveNumber) (const "") (moves pgn)
+    mlist = map stripMarks . init . T.words $ streamEdit (extraInfo <|> moveNumber) (const "") (moves pgn)
 
-    extraInfo = TextP.char '{' *> TextP.takeTill ((==) '}') <* TextP.char '}'
+    extraInfo = TextP.char '{' *> TextP.takeTill ('}' ==) <* TextP.char '}'
     moveNumber = do
-        TextP.decimal
-        (TextP.many1 $ TextP.char '.')
-        return ""
+      (_ :: Int) <- TextP.decimal
+      TextP.many1 $ TextP.char '.'
+      return ""
     stripMarks = T.filter (\c -> c /= '?' && c /= '!')
 
     l = length mlist
-    moves' = T.intercalate "," $ Prelude.take n $ mlist
-    moves'' = if l >= n then moves' -- case enough moves
-              else if l == 0 then (T.concat $ Prelude.take (n-l-1) $ repeat ",") -- case no moves
-              else moves' <> (T.concat $ Prelude.take (n - l) $ repeat ",") -- case not enough moves
+    moves' = T.intercalate "," $ Prelude.take n mlist
+    moves''
+      | l >= n = moves'
+      | l == 0 = T.concat $ replicate (n - l - 1) "," -- case no moves
+      | otherwise = moves' <> T.concat (replicate (n - l) ",") -- case not enough moves
 
-cutMoves :: Int -> PGN -> PGN
-cutMoves n pgn =
-    pgn { moves = moves'' }
-  where
-    mlist = T.words $ moves pgn
-    l = length mlist
-    moves' = T.intercalate "," $ Prelude.take n $ mlist
-    moves'' = if l >= n then moves' else if l == 0 then (T.concat $ Prelude.take (n-l-1) $ repeat ",") else moves' <> (T.concat $ Prelude.take (n - l) $ repeat ",")
-
+-- cutMoves :: Int -> PGN -> PGN
+-- cutMoves n pgn =
+--   pgn {moves = moves''}
+--   where
+--     mlist = T.words $ moves pgn
+--     l = length mlist
+--     moves' = T.intercalate "," $ Prelude.take n mlist
+--     moves''
+--       | l >= n = moves'
+--       | l == 0 = T.concat $ replicate (n - l - 1) ","
+--       | otherwise = moves' <> T.concat (replicate (n - l) ",")
 
 processPGN :: PGN -> PGN
 processPGN pgn =
-    pgn { result = result', whiteElo = whiteElo', blackElo = blackElo' }
+  pgn {result = result', whiteElo = whiteElo', blackElo = blackElo'}
   where
     result' = case result pgn of
-      "1-0"     -> "1"
-      "0-1"     -> "-1"
+      "1-0" -> "1"
+      "0-1" -> "-1"
       "1/2-1/2" -> "0"
-      _         -> ""
+      _ -> ""
     whiteElo' = case whiteElo pgn of
       "?" -> "0"
-      _   -> whiteElo pgn
+      _ -> whiteElo pgn
     blackElo' = case blackElo pgn of
       "?" -> "0"
-      _   -> blackElo pgn
+      _ -> blackElo pgn
 
 colNames :: [Text]
-colNames = ["site","date","white","black","whiteElo","blackElo","eco","timeControl","result","termination","variant"]
+colNames = ["site", "date", "white", "black", "whiteElo", "blackElo", "eco", "timeControl", "result", "termination", "variant"]
 
 formatToCSV :: Text -> PGN -> Text
 formatToCSV sep pgn =
-    site pgn <> sep <> date pgn <> sep <> whitePlayer pgn <> sep <> blackPlayer pgn <> sep <> whiteElo pgn <> sep <> blackElo pgn <> sep <> eco pgn <> sep <> timeControl pgn <> sep <> result pgn <> sep <> termination pgn <> sep <> variant pgn <> sep <> moves pgn
+  site pgn <> sep <> date pgn <> sep <> whitePlayer pgn <> sep <> blackPlayer pgn <> sep <> whiteElo pgn <> sep <> blackElo pgn <> sep <> eco pgn <> sep <> timeControl pgn <> sep <> result pgn <> sep <> termination pgn <> sep <> variant pgn <> sep <> moves pgn
